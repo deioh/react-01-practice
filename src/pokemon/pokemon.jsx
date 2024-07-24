cab1da88942d2cd3017ebc88ef61dc67b30a21e4
deioh <143625578+deioh@users.noreply.github.com>
2024-07-24 23:20:08 +0800

    update

--- src/pokemon/pokemon.jsx
+++ src/pokemon/pokemon.jsx
@@ -2,22 +2,25 @@ import pokemonData from '../pokemon/pokemonData.json';
 import styles from './pokemon.module.css';
 export const PokemonList = () => {
   return (
-    <div>
+    <div className={styles.div}>
       {pokemonData.map(pokemon => (
-        <div key={pokemon.id}>
+        <div className={styles.box} key={pokemon.id}>
           <p>ID: {pokemon.id}</p>
           <p>Name: {pokemon.name}</p>
           <p>
-            URL: {pokemon.url}
-            {pokemon.id}
+            <a
+              href={`https://pokemondb.net/pokedex/${pokemon.name}`}
+              target="_blank"
+            >
+              <img
+                src={`https://img.pokemondb.net/artwork/${pokemon.name}.jpg`}
+                width="360"
+                height="336"
+                alt="Artwork by Ken Sugimori"
+                fetchpriority="high"
+              ></img>
+            </a>
           </p>
-          <img
-            src={`https://img.pokemondb.net/artwork/${pokemon.name}.jpg`}
-            width="360"
-            height="336"
-            alt="Bulbasaur artwork by Ken Sugimori"
-            fetchpriority="high"
-          ></img>
         </div>
       ))}
     </div>
